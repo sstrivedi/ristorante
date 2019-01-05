15a8429667b17635ed4513dc5c0bb6144e4aefd6
S Trivedi <shaptrivedi@gmail.com>
2019-01-04 23:09:45 -0500

    Updates

---
+++ precache-manifest.6b8d1e6c5cb523171d8bfc69ad703f24.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ristorante/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "5682a152baeabfaa4057",
+    "url": "/ristorante/static/css/main.146d433d.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ristorante/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "e8d028d7ff574e6a2ad5",
+    "url": "/ristorante/static/js/1.e8d028d7.chunk.js"
+  },
+  {
+    "revision": "6683115b3166ddb8248e",
+    "url": "/ristorante/static/js/runtime~main.6683115b.js"
+  },
+  {
+    "revision": "5682a152baeabfaa4057",
+    "url": "/ristorante/static/js/main.5682a152.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ristorante/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ristorante/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ristorante/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "e8d028d7ff574e6a2ad5",
+    "url": "/ristorante/static/css/1.e9c3ab66.chunk.css"
+  },
+  {
+    "revision": "498d73284a5960a49a49af39af0efcdd",
+    "url": "/ristorante/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ristorante/precache-manifest.6b8d1e6c5cb523171d8bfc69ad703f24.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ristorante/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
